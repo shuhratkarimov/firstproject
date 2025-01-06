5c99d592b76da1affe93658d1312f33e5a630d5f
shuhrat <n395b421na@gmail.com>
2025-01-06 18:14:41 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const connectDB = require('./config');
+const PORT = process.env.PORT || 4000
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    grade: String,
+});
+
+const Student = mongoose.model('students', studentSchema);
+
+// Create
+app.post('/students', async (req, res) => {
+    const student = new Student(req.body);
+    await student.save();
+    res.status(201).json(student);
+});
+
+// Read
+app.get('/students', async (req, res) => {
+    const students = await Student.find();
+    res.json(students);
+});
+
+// Update
+app.put('/students/:id', async (req, res) => {
+    const student = await Student.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(student);
+});
+
+// Delete
+app.delete('/students/:id', async (req, res) => {
+    await Student.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
